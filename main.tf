e22511723a14191639cf066f9be6ebc006f39ae1
vjsmit <vj.smit97@gmail.com>
2023-08-05 14:21:32 +0530

    updating userdata.sh

--- main.tf
+++ main.tf
@@ -31,3 +31,19 @@ resource "aws_lb" "main" {
   subnets            = var.subnets
   tags =  merge ({ Name = "${var.name}-${var.env}-lb" }, var.tags)
 }
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = var.port
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default error"
+      status_code  = "500"
+    }
+  }
+}
