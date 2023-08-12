7b97ad84102843349c2831a3eed8e308854681f0
vjsmit <vj.smit97@gmail.com>
2023-08-12 09:17:22 +0530

    updating userdata.sh

--- main.tf
+++ main.tf
@@ -38,7 +38,8 @@ resource "aws_lb" "main" {
   tags =  merge ({ Name = "${var.name}-${var.env}-lb" }, var.tags)
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "public" {
+  count = var.name == "public" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
@@ -54,7 +55,25 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+resource "aws_lb_listener" "private" {
+  count = var.name == "private" ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default error"
+      status_code  = "500"
+    }
+  }
+}
+
 resource "aws_lb_listener" "main" {
+  count = var.name == "public" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = 443
   protocol          = "HTTPS"
